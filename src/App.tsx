49f10d577881db4667d53601311f868254a61b6e
Everton Christmann <evertonluiz00@hotmail.com>
2021-12-21 21:18:47 -0300

    Created Context Api

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 import { GlobalStyle } from "./styles/global";
+
 
 Modal.setAppElement('#root');
 
@@ -20,11 +22,13 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
+
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
-    </>
+
+    </TransactionsProvider>
   );
 }
